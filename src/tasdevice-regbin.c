f526b323fd8e866efd45ed4dbdebcfe32ff001eb
Shenghao Ding <shenghao-ding@ti.com>
2023-01-02 22:25:48 +0800

    remove unused space and add 2023 into the lisence

--- src/tasdevice-regbin.c
+++ src/tasdevice-regbin.c
@@ -1,10 +1,10 @@
 /*
  * TAS2871 Linux Driver
  *
- * Copyright (C) 2022 Texas Instruments Incorporated 
- * 
+ * Copyright (C) 2022 - 2023 Texas Instruments Incorporated
+ *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as 
+ * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation version 2.
  *
  * This program is distributed "as is" WITHOUT ANY WARRANTY of any
