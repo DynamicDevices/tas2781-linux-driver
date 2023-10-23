006b79f2ccc1f14bafd0d7a03ab9856366d16874
Ding <shenghao-ding@ti.com>
2023-10-23 12:23:58 +0800

    fixed page refresh in regmap during switching device
    Signed-off-by: Ding <shenghao-ding@ti.com>

--- src/tasdevice-rw.c
+++ src/tasdevice-rw.c
@@ -160,9 +160,7 @@ static int tasdevice_change_chn_book(
 		if(addr_chg == false && tasdev->cur_book == book)
 			goto out;
 
-		if (tasdev->cur_book != book)
-		/* addr_chg || tasdev->cur_book != book */
-		ret =  tasdevice_regmap_write(tas_priv,
+		ret = tasdevice_regmap_write(tas_priv,
 					TASDEVICE_PAGE_SELECT, 0);
 		if (ret < 0) {
 			dev_err(tas_priv->dev, "%s, E=%d\n",
